ce99e8c4f93f033787361594f3fe24310413220d
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-05-15 00:41:57 +0100

    ex 2-1-2

---
+++ introtoalgorithms-java/src/main/java/com/mounacheikhna/introtoalgorithms/chapter1/Ex2_1_2.java
@@ -0,0 +1,25 @@
+package com.mounacheikhna.introtoalgorithms.chapter1;
+
+import java.util.Arrays;
+
+public class Ex2_1_2 {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 11, 2, 19, 6, 23};
+        Ex2_1_2.insertSortDec(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void insertSortDec(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int val = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] < val) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = val;
+        }
+    }
+
+}
