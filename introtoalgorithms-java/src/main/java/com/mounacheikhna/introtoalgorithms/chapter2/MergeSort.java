df1651fab463ed2ee59d4350c30d01f42abaf779
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-05-19 18:22:58 +0100

    Solution to merge sort.

--- introtoalgorithms-java/src/main/java/com/mounacheikhna/introtoalgorithms/chapter2/MergeSort.java
+++ introtoalgorithms-java/src/main/java/com/mounacheikhna/introtoalgorithms/chapter2/MergeSort.java
@@ -22,10 +22,10 @@ public class MergeSort {
         int middle = (leftStart + rightEnd) / 2;
         mergesort(array, temp, leftStart, middle);
         mergesort(array, temp, middle + 1, rightEnd);
-        merge(array, temp, leftStart, rightEnd);
+        clrsMergeWithSentinel(array, temp, leftStart, rightEnd);
     }
 
-    public static void merge(int[] array, int[] temp, int leftStart, int rightEnd) {
+    public static void clrsMergeWithSentinel(int[] array, int[] temp, int leftStart, int rightEnd) {
         int leftEnd = (leftStart + rightEnd) / 2;
         int rightStart = leftEnd + 1;
         int size = rightEnd - leftStart + 1;
@@ -49,4 +49,88 @@ public class MergeSort {
         System.arraycopy(array, right, temp, index, rightEnd - right + 1);
         System.arraycopy(temp, leftStart, array, leftStart, size);
     }
+
+    /**
+     * Merge two sorted sublists into a single list
+     * (Version from the book CLRS with sentinel)
+     *
+     * @param A a list containing two sorted sublists
+     * @param p start index of first list
+     * @param q end index of first list
+     * @param r end index of second list
+     */
+    @SuppressWarnings("ManualArrayCopy")
+    public static void clrsMergeWithSentinel(int[] A, int p, int q, int r) {
+        int n1 = q - p + 1;
+        int n2 = r - q;
+
+        int[] L = new int[n1 + 1];
+        int[] R = new int[n2 + 1];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = A[p + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = A[q + 1 + j];
+        }
+
+        L[n1] = Integer.MAX_VALUE;
+        R[n2] = Integer.MAX_VALUE;
+
+        int i = 0;
+        int j = 0;
+
+        // sentinel values allow skipping empty checks
+        for (int k = p; k <= r; k++) {
+            if (L[i] <= R[j]) {
+                A[k] = L[i];
+                i++;
+            } else {
+                A[k] = R[j];
+                j++;
+            }
+        }
+    }
+
+    // ex 2.3-2
+    public static void clrsMergeWithoutSentinel(int[] A, int p, int q, int r) {
+        int n1 = q - p + 1;
+        int n2 = r - q;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = A[p + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = A[q + 1 + j];
+        }
+
+        int i = 0;
+        int j = 0;
+        int k = p;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                A[k] = L[i];
+                i++;
+            } else {
+                A[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        for (int i1 = i; i1 < L.length; i1++) {
+            A[k] = L[i1];
+            k++;
+        }
+        for (int j1 = j; j1 < R.length; j1++) {
+            A[k] = R[j1];
+            k++;
+        }
+
+    }
+
 }
